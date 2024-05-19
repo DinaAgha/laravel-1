5cc64733ebdd7f5d3331784b3307625d7322ad5d
DinaAgha <168042700+DinaAgha@users.noreply.github.com>
2024-05-19 23:52:01 +0300

    task6

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,32 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Student;
+use APP\Models\Student;
 
 class StudentController extends Controller
 {
-    public function create()
-    {
-        return view('students');
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required',
-            'student_id' => 'required',
-            'age' => 'required|integer',
-        ]);
-
-        Student::create($request->all());
-
-        return redirect()->route('students.index')
-                         ->with('success', 'Student created successfully.');
-    }
-
+    private $columns = ['name','student_id','age'];
     public function index()
     {
-        $students = Student::all();
-        return view('students', compact('students'));
+        $student = Student::get();
+        return view('student', compact('student'));
     }
+     public function create()
+    {
+        return view('addStudent');
+    }
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|max:100|min:5',
+            'student_id'=> 'required|<min:11>',
+            'age'=>'required',
+        ]);
+        Student::create($data);
+        return redirect('student');
+    }
+    public function show(string $id)
+    {
+        $student = Student::findOrFail($id);
+        return view('showStudent',compact('student'));
+    }
+ 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,49 @@
 <?php
-
-use App\Http\Controllers\MyController;
+namespace App\Http\Controllers;
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ClientController;
-use APP\Http\Controllers\FrontController;
-use App\Http\Controllers\students2Controller;
-
-
-Route::Post('insertStudent',[students2Controller::class,'store'])->name('insertStudent');
-Route::get('addStudent',[students2Controller::class,'create'])->name('addStudent');
-Route::get('students',[students2Controller::class,'index'])->name('students');
-
-Route::Post('insertClients',[ClientController::class,'store'])->name('insertClient');
-Route::get('addClient',[ClientController::class,'create'])->name('addClient');
-Route::get('clients',[ClientController::class,'index'])->name('clients');
-
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\MyController;
+
+use App\Http\Controllers\ClientController;
+
+
+
+
+Route::post('insertStudent',[StudentController::class,'store'])->name('insertStudent');
+Route::get('addStudent',[StudentController::class,'create'])->name('addStudent');
+Route::get('student',[StudentController::class,'index'])->name('student');
+Route::get('editStudent/{id}',[StudentController::class,'edit'])->name('editClient');
+Route::put('updateStudent/{id}',[StudentController::class,'update'])->name('updateStudent');
+Route::get('showStudent',[StudentController::class,'show'])->name('showStudent');
+
+
+Route::post('insertClients',[ClientController::class,'store'])->name('insertClient');
+Route::get('addClient',[ClientController::class,'create'])->name('addClient');
+Route::get('client',[ClientController::class,'index'])->name('client');
+Route::get('editClient/{id}',[ClientController::class,'edit'])->name('editClient');
+Route::put('updateClient/{id}',[ClientController::class,'update'])->name('updateClient');
+Route::get('showClient/{id}',[ClientController::class,'show'])->name('showClient');
+Route::delete('delClient',[ClientController::class,'destroy'])->name('delClient');
+Route::delete('forceDelete',[ClientController::class,'forceDelete'])->name('forceDelete');
+Route::get('trashClient',[ClientController::class,'trash'])->name('trashClient');
+Route::get('restoreClient/{id}',[ClientController::class,'restore'])->name('restoreClient');
 
 // Route::get('/students', [StudentController::class, 'create'])->name('students.create');
 // Route::post('/students', [StudentController::class, 'store'])->name('students.store');
 // Route::get('/students', [StudentController::class, 'index'])->name('students.index');
 
-Route::get('/car/{name?}', function ($name=null) {
-    return 'The name is: ' . $name;
-    })->whereAlpha('name');
+// Route::get('/car/{name?}', function ($name=null) {
+//     return 'The name is: ' . $name;
+//     })->whereAlpha('name');
 
-Route::get('Dina/',function() {
-return 'welcome to my first website';
-});
+// Route::get('Dina/',function() {
+// return 'welcome to my first website';
+// });
 
-Route::get('Ali/{id?}',function($id=0){
-    return 'welcome'.$id;
-})->whereNumber('id');
+// Route::get('Ali/{id?}',function($id=0){
+//     return 'welcome'.$id;
+// })->whereNumber('id');
 
 // Route::get('course/{name}', function($name){
 //     return $name.' course';
@@ -45,23 +57,23 @@ Route::get('Ali/{id?}',function($id=0){
 // ->group(function(){
 //     Route::get('my_data');
 // });
-Route::prefix('course')->group(function (){ 
-    Route::get('max', function(){
-        return 'Course you have rolled in is 3DsMAX';
-});
+// Route::prefix('course')->group(function (){ 
+//     Route::get('max', function(){
+//         return 'Course you have rolled in is 3DsMAX';
+// });
 
-Route::get('cad',function(){
-    return 'Cpurse you have rolled in is CAD';
-});
-});
+// Route::get('cad',function(){
+//     return 'Cpurse you have rolled in is CAD';
+// });
+// });
 
-Route::fallback(function(){
-    return redirect('/Dina');
-});
+// Route::fallback(function(){
+//     return redirect('/Dina');
+// });
 
-Route::get('ex', function(){
-    return view('cars');
-    });
+// Route::get('ex', function(){
+//     return view('cars');
+//     });
 
 Route::post('ex',function(){
     return'Data recieved';
